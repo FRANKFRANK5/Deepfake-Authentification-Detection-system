e1a42ebaca75228a3ca55b1d8c9eeee6ea2765ec
FRANKFRANK5 <272574425+FRANKFRANK5@users.noreply.github.com>
2026-06-28 23:55:52 +0300

    Update ALLOWED_HOSTS for Render domain detectify-ywlk.onrender.com

--- project_settings/settings.py
+++ project_settings/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '@)0qp0!&-vht7k0wyuihr+nk-b8zrvb5j^1d@
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 # Change and set this to correct IP/Domain
-ALLOWED_HOSTS = ['detectify.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['detectify.onrender.com', 'localhost', '127.0.0.1', 'detectify.onrender.com']
 
 
 # Application definition
